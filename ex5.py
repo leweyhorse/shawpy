cbf8621554015869ca0bcfe4ff303d24326bc450
terry <terry@terrys-Mac-Pro.local>
2020-10-04 13:28:15 -0600

    First commit

---
+++ ex5.py
@@ -0,0 +1,8 @@
+# ex5.py
+
+my_name = 'Lew the horse'
+my_age = 21
+my_bd = 1998
+print "I am %s." % my_name
+print "My age was %d." % my_age
+print "Add %d and %d to get %d." % (my_age, my_bd, my_age + my_bd)
---
+++ test.py
@@ -0,0 +1,9 @@
+# A comment
+
+print "I could have code like this." # and the comment is ignored
+
+# You can comment out code
+
+# print "This won't run."
+
+print "This will run."
